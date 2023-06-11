42f2d0897a0011219052d685f09c4224cb7d2694
RavindraKinagi <107475370+RavindraKinagi@users.noreply.github.com>
2023-06-11 19:26:55 +0530

    Update dataprep.py

--- classification_iris/dataprep.py
+++ classification_iris/dataprep.py
@@ -1,1 +1,13 @@
+# Drop unnecessary columns
+X = data.drop(['Id', 'Species'], axis=1)
+y = data['Species']
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=5)
+
+# Print the shapes of the training and testing sets
+print(X_train.shape)
+print(y_train.shape)
+print(X_test.shape)
+print(y_test.shape)
 
