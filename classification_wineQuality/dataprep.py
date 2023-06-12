c0b1140e290981e9aff458a3c630b8c97b202ce1
RavindraKinagi <107475370+RavindraKinagi@users.noreply.github.com>
2023-06-12 11:46:08 +0530

    Update dataprep.py

--- classification_wineQuality/dataprep.py
+++ classification_wineQuality/dataprep.py
@@ -1,1 +1,105 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers, Sequential
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+# Importing the data
+df = pd.read_csv("../input/wine-quality/winequalityN.csv")
+
+# Preprocess the data
+df = df.dropna()
+df['is_white_wine'] = [1 if typ == 'white' else 0 for typ in df['type']]
+df['is_good_wine'] = [1 if quality >= 6 else 0 for quality in df['quality']]
+df.drop(['type', 'quality'], axis=1, inplace=True)
+
+# Split into test and train
+X = df.drop('is_good_wine', axis=1)
+y = df['is_good_wine']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Transform the data
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Creating the basic model
+model = tf.keras.Sequential([
+    layers.Flatten(input_shape=(12,)),
+    layers.Dense(128, activation='relu'),
+    layers.Dropout(0.2),
+    layers.Dense(1, activation='sigmoid')
+])
+
+# Compiling the model
+model.compile(
+    loss='binary_crossentropy',
+    optimizer='adam',
+    metrics='accuracy'
+)
+
+# Fitting the model
+history = model.fit(
+    X_train_scaled,
+    y_train,
+    epochs=100,
+    callbacks=[
+        tf.keras.callbacks.LearningRateScheduler(
+            lambda epoch: 1e-3 * 10 ** (epoch / 30)
+        )
+    ]
+)
+
+# Plotting the model
+plt.plot(
+    np.arange(1, 101),
+    history.history['loss'],
+    label='Loss', lw=3
+)
+plt.plot(
+    np.arange(1, 101),
+    history.history['accuracy'],
+    label='Accuracy', lw=3
+)
+plt.plot(
+    np.arange(1, 101),
+    history.history['lr'],
+    label='Learning rate', color='#000', lw=3, linestyle='--'
+)
+plt.title('Evaluation metrics', size=20)
+plt.xlabel('Epoch', size=14)
+plt.legend();
+
+# Compiling the model with different optimizer values
+model.compile(
+    loss=tf.keras.losses.binary_crossentropy,
+    optimizer=tf.keras.optimizers.Adam(learning_rate=0.007),
+    metrics=[
+        tf.keras.metrics.BinaryAccuracy(name='accuracy')
+    ]
+)
+
+history_optimized = model.fit(
+    X_train_scaled,
+    y_train,
+    epochs=100
+)
+
+# Plotting the data
+plt.plot(
+    np.arange(1, 101),
+    history_optimized.history['loss'],
+    label='Loss', lw=3
+)
+plt.plot(
+    np.arange(1, 101),
+    history_optimized.history['accuracy'],
+    label='Accuracy', lw=3
+)
+plt.title('Accuracy vs. Loss per epoch', size=20)
+plt.xlabel('Epoch', size=14)
+plt.legend()
 
