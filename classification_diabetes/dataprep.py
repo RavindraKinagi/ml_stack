efeb86f05661a0759fe451b684c2b4eadca160ff
RavindraKinagi <107475370+RavindraKinagi@users.noreply.github.com>
2023-06-12 11:09:57 +0530

    Update dataprep.py

--- classification_diabetes/dataprep.py
+++ classification_diabetes/dataprep.py
@@ -1,1 +1,9 @@
+assembler = VectorAssembler(inputCols=df.columns[:-1], outputCol="assembled")
+assembler_features = assembler.transform(df).select("assembled", "Outcome")
+
+assembler_features.toPandas().head()
+scaler = StandardScaler(inputCol='assembled', outputCol='features')
+df_scaled = scaler.fit(assembler_features).transform(assembler_features).select("features", "Outcome")
+
+df_scaled.toPandas().head()
 
